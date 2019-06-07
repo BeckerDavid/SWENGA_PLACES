444fa99d8bbb6b0e32ef88c9e89582e6dfba840d
Accalia44 <Accalia44@users.noreply.github.com>
2019-06-07 11:20:25 +0200

    Dependencies upgedated, Contorller erstsellt, dispatcher erstellt

---
+++ Places/src/at/fh/swenga/places/dao/UserDAO.java
@@ -0,0 +1,18 @@
+package at.fh.swenga.places.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.fh.swenga.places.model.UserModel;
+
+
+
+@Repository
+@Transactional
+public interface UserDAO extends JpaRepository<UserModel, Integer>{
+	
+
+
+}
+
