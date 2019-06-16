2fde382a008fce50e226ce3f378961c01fdd729b
YellowIcicle <michaelderler@gmx.net>
2019-06-16 16:40:06 +0200

    logout fix + profile button

--- Places/src/at/fh/swenga/places/controller/PlacesControler.java
+++ Places/src/at/fh/swenga/places/controller/PlacesControler.java
@@ -2,15 +2,20 @@ package at.fh.swenga.places.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import at.fh.swenga.places.dao.CountryRepository;
 import at.fh.swenga.places.dao.RecommendationRepository;
@@ -148,6 +153,15 @@ public class PlacesControler {
 
 	  return"error";
 
+	 }
+	 
+	 @RequestMapping(value="/logout", method = RequestMethod.GET)
+	 public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	     Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	     if (auth != null){    
+	         new SecurityContextLogoutHandler().logout(request, response, auth);
+	     }
+	     return "redirect:/login?logout";
 	 }
 	
 
